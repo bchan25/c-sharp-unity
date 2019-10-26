d07b00e69ef4810c69b05c649448e7e8c140dd58
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-26 10:56:05 +0100

    Variables notes added

---
+++ Assets/Scripts/variables/Variables.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    /// <summary>
+    /// public or private reference
+    /// data type (string, int, float, bool) or Unity Components
+    /// name
+    /// optional -- a value
+    /// </summary>
+    ///
+
+    // Public can be seen in Inspector
+    public string myName = "Ben";
+    // Can't be access by other scripts
+    private int myAge = 31;
+    public string myLocation = "Chester";
+
+    private void Start()
+    {
+        // Value can change
+        myAge += 1;
+
+        // Print using variable
+        Debug.Log("Hello " + myName + ". Your " + myAge + " years old and from " + myLocation + ".");
+    }
+}
