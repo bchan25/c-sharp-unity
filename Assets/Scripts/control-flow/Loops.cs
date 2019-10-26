052ec46ca38a3eb4e61e9e38b238813567bbdda3
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-26 12:00:49 +0100

    Loops notes added

---
+++ Assets/Scripts/control-flow/Loops.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+	public string name = "Ben";
+
+    // Loops good to use with arrays
+
+	void Start()
+	{
+		// Repeating code based on condition
+		for (int i = 0; i < 5; i++)
+		{
+			Debug.Log("Hello " + name);
+		}
+
+		for (int i = 0; i < 100; i++)
+		{
+            if(i == 50)
+			{
+                // This will stop the loop early
+				break;
+			}
+			Debug.Log(i);
+		}
+
+
+        // Foreach used with Arrays and Collections
+
+        int x = 0;
+
+        // Loop that run at least once first
+        /* 
+        do
+        {
+            Debug.Log("I print at least once");
+            x++;
+        } while (x < 3);
+        */
+
+
+        while(x < 5)
+        {
+            Debug.Log(x);
+            x++;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/control-flow/loop-program/AddApples.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddApples : MonoBehaviour
+{
+    public int apples;
+
+    
+    void Start()
+    {
+        StartCoroutine(AddApplesRoutine());    
+    }
+
+
+    // Great for pausing code
+    IEnumerator AddApplesRoutine()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            apples++;
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+}
