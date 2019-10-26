3e66073aafa6677afee13ee38cd0765fd57b0b01
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-26 11:38:29 +0100

    Switch Logic notes added

---
+++ Assets/Scripts/control-flow/SwitchLogic.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchLogic : MonoBehaviour
+{
+    private int selectedLevel;
+
+    private int _easy = 0;
+    private int _medium = 1;
+    private int _hard = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedLevel = Random.Range(0, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (selectedLevel)
+        {
+            case 0:
+                Debug.Log("Easy!");
+                break;
+            case 1:
+                Debug.Log("Medium");
+                break;
+            case 2:
+                Debug.Log("Hard!!");
+                break;
+            default:
+                Debug.Log("Invalid Selection");
+                break;
+        }
+
+        // Input to set random level
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            selectedLevel = Random.Range(0, 3);
+        }
+
+        // Input to Invalid Code
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            selectedLevel = 55;
+        }
+    }
+}
