00f7cc54f067a1d30821cab78b53f4bd84106b87
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-26 12:22:31 +0100

    Array notes added

---
+++ Assets/Scripts/collection/ArraySyntax.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraySyntax : MonoBehaviour
+{
+	// Array hold group of data in one variable name
+	public string[] names;
+
+
+    
+    void Start()
+    {
+		// Access the data
+		Debug.Log(names[0]);
+
+    }
+
+    
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+            // Looping Array
+            foreach(var item in names)
+            {
+                Debug.Log("Hello " + item);
+            }
+		}        
+    }
+}
