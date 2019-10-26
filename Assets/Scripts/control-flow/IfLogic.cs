aa1022677dad777f2a0fdd50fd78651721df1f35
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-26 11:17:13 +0100

    If-else Logic notes added

---
+++ Assets/Scripts/control-flow/IfLogic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfLogic : MonoBehaviour
+{
+
+    public int myAge = 31;
+
+    void Start()
+    {
+        /*
+        if (CONDITION)
+        {
+            CODE
+        }
+        */
+
+        if (myAge >= 17)
+        {
+            Debug.Log("You can drive");
+        }
+        else if (myAge < 0)
+        {
+            Debug.Log("Not a real age");
+        }
+        else
+        {
+            Debug.Log("You can't drive");
+        }
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Spacebar Hit");
+        }
+    }
+}
--- Assets/Scripts/variables/Variables.cs
+++ Assets/Scripts/variables/Variables.cs
@@ -12,18 +12,19 @@ public class Variables : MonoBehaviour
     /// </summary>
     ///
 
+
     // Public can be seen in Inspector
     public string myName = "Ben";
     // Can't be access by other scripts
-    private int myAge = 31;
+    private int _myAge = 31;
     public string myLocation = "Chester";
 
     private void Start()
     {
         // Value can change
-        myAge += 1;
+        _myAge += 1;
 
         // Print using variable
-        Debug.Log("Hello " + myName + ". Your " + myAge + " years old and from " + myLocation + ".");
+        Debug.Log("Hello " + myName + ". Your " + _myAge + " years old and from " + myLocation + ".");
     }
 }
