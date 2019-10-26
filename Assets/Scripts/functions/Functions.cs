44a99f280e7469c2e01a41811ad0a9f0b7edce6f
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-26 12:43:15 +0100

    Functions (Methods) notes added

---
+++ Assets/Scripts/functions/Functions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+    public int answer;
+
+    void Start()
+    {
+        // Call Method
+        SayHello();
+        Shout("HELP!");
+
+        answer = Add(5, 4);
+    }
+
+    
+
+    // Your Method
+    private void SayHello()
+    {
+        Debug.Log("Hello");
+    }
+
+    private void Shout(string word)
+    {
+        Debug.Log(word);
+    }
+
+    private int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+}
